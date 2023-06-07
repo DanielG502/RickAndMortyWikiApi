9d5abb13555ffa1f38426bf66d36ffdac790ef8c
Jorge Gonzalez <jdanielgonzaleze@gmail.com>
2023-06-07 17:17:20 -0600

    Creating the part of the data we get from the API

---
+++ src/components/Cards/Cards.js
@@ -0,0 +1,13 @@
+import React from 'react'
+
+export const Cards = () => {
+    return (
+        <div className='col-4'>Cards</div>
+    )
+}
+
+// <div className="col-4">Cards</div>
+// <div className="col-4">Cards</div>
+// <div className="col-4">Cards</div>
+
+export default Cards
